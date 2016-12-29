1cea2488a2ea1e8ac945de1cf838916c9fd0a24b
sohailRa <sohail.ra5@gmail.com>
2016-12-29 15:24:41 -0500

    Adding OddEven/Simple_cal

---
+++ main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+
+void check_oddEven();
+void simple_calculator();
+
+int main(){
+
+	//check_oddEven();
+	simple_calculator();
+	return 0;
+}
+
+
+void check_oddEven(){
+
+	int num;
+
+	printf("Enter a number: ");
+	scanf("%d", &num);
+
+	if(num ==0)
+		printf("\nThe number is zero\n");
+	else if (num%2 == 0)
+		printf("\nThe number is even\n");
+	else
+		printf("\nThe number is odd\n");
+}
+
+
+void simple_calculator(){
+
+	int num1, num2, choice;
+
+	printf("Enter two numbers: ");
+	scanf("%d %d", &num1, &num2);
+
+	printf("\n1 Addition");
+	printf("\n2 Subtraction");
+	printf("\n3 Multiplication");
+	printf("\n4 Division");
+
+	printf("\nEnter Choice: ");
+	scanf("%d", &choice);
+
+	switch(choice){
+		case 1:
+			printf("\n%d + %d = %d\n", num1, num2, num1+num2);
+			break;
+		case 2:
+			printf("\n%d - %d = %d\n", num1, num2, num1-num2);
+			break;
+		case 3:
+			printf("\n%d x %d = %d\n", num1, num2, num1*num2);
+			break;
+		case 4:
+			printf("\n%d / %d = %d\n", num1, num2, num1/num2);
+			break;
+		default:
+			printf("\nThis operaton is not possible\n");
+	}
+
+}
